f357c0afe8d9ba7d1fecda62d667ddfb07135c48
nivitha2 <nivithamavuluri@gmail.com>
2025-02-26 20:19:04 -0800

    Update ME_210_Final_Project_sensors.ino

--- ME_210_Final_Project_sensors.ino
+++ ME_210_Final_Project_sensors.ino
@@ -5,6 +5,14 @@
  */
 float getUltraSonicBack()
 {
+  digitalWrite(trigBack, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigBack, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigBack, LOW);
+  float duration = pulseIn(echoBack, HIGH);
+  float distance = (duration * 0.0343) / 2; // Convert to cm
+  return distance;
 
 }
 
@@ -13,6 +21,14 @@ float getUltraSonicBack()
  */
 float getUltraSonicFront()
 {
+ digitalWrite(trigFront, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigFront, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigFront, LOW);
+  float duration = pulseIn(echoFront, HIGH);
+  float distance = (duration * 0.0343) / 2; // Convert to cm
+  return distance;
 
 }
 
@@ -21,6 +37,14 @@ float getUltraSonicFront()
  */
 float getUltraSonicRight()
 {
+  digitalWrite(trigLeft, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigLeft, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigLeft, LOW);
+  float duration = pulseIn(echoLeft, HIGH);
+  float distance = (duration * 0.0343) / 2; // Convert to cm
+  return distance;
 
 }
 
